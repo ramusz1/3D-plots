07dc22e556406cddcc1019199deef61c93ee50df
ramusz1 <ramusz@wp.pl>
2018-06-02 16:46:35 +0200

    working basic graph drawing

--- z1.js
+++ z1.js
@@ -3,23 +3,26 @@ function getPoints(minX,maxX,minZ,maxZ,n,func){
   let points = [];
   let stepX = (maxX - minX) / n;
   let stepZ = (maxZ - minZ) / n;
-  let minY = Math.MAX_VALUE;
-  let maxY = -Math.MAX_VALUE;
+  let minY = Number.MAX_VALUE;
+  let maxY = -Number.MAX_VALUE;
   for(let x = 0; x < n; x ++){
     for(let z = 0; z < n ; z ++ ){
-      let y = func(x,z);
+      let realX = x * stepX + minX;
+      let realZ = z * stepZ + minZ;
+      let y = func(realX,realZ);
       if (maxY < y) {
-        max = y;
+        maxY = y;
       }
       if (minY > y) {
-        min = y;
+        minY = y;
       }
-      points = points.concat([x,y,z]);
+      points.push(realX);
+      points.push(y);
+      points.push(realZ);
     }
   }
   return {points : points,minY : minY,maxY : maxY};
 }
-
 
 function main() {
 
@@ -41,9 +44,9 @@ function main() {
   // lines
   var coordsSys = {};
   coordsSys.setData = function(minX,maxX,minY,maxY,minZ,maxZ){
-    let midX = (minX + maxX) / 2;
-    let midY = (minY + maxY) / 2;
-    let midZ = (minZ + maxZ) / 2;
+    let midX = 0; //(minX + maxX) / 2;
+    let midY = 0; //(minY + maxY) / 2;
+    let midZ = 0; //(minZ + maxZ) / 2;
     coordsSys.data = [
       minX,midY,midZ,
       maxX,midY,midZ,
@@ -55,8 +58,6 @@ function main() {
   coordsSys.dims = 3;
   coordsSys.primitive = gl.LINES;
   coordsSys.buff = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, coordsSys.buff);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coordsSys.data), gl.STATIC_DRAW);
   coordsSys.color = [0,0,0,1];
 
   var points = {};
@@ -64,21 +65,26 @@ function main() {
   points.dims = 3;
   points.primitive = gl.POINTS;
   points.buff = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, points.buff);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(points.data), gl.STATIC_DRAW);
-  points.color = [0.7,0.3,0.3,1];
+  points.color = [0.7,0.3,0.3,0.7];
 
   const positionLocation = gl.getAttribLocation(program,'a_position');
   const transformLocation = gl.getUniformLocation(program,'transform');
   const colorLocation = gl.getUniformLocation(program, "u_color");
 
   // code above is for initialization
+  var transform = [];
+  ratio = canvas.height / canvas.width;
+  var perspective = mat.perspective(Math.PI/2.1,2,20,ratio);
   var redraw = function(){
 
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     gl.enable( gl.DEPTH_TEST);
+
+    gl.enable(gl.BLEND);
+    gl.blendFunc(gl.SRC_ALPHA, gl.ONE);
+
     gl.useProgram(program);
 
     var size = 3;          
@@ -86,17 +92,19 @@ function main() {
     var normalize = false; 
     var stride = 0;        
     var offset = 0;
-            
+    
+    var finalTransform = mat.multiply(perspective,transform);
+
     drawObject = function( object ){
       // draw ball
-      console.log(object.data);
       gl.bindBuffer(gl.ARRAY_BUFFER, object.buff );
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(object.data), gl.STATIC_DRAW);
       gl.vertexAttribPointer(
           positionLocation, size, type, normalize, stride, offset)
       gl.enableVertexAttribArray(positionLocation);
-      gl.uniformMatrix4fv(transformLocation, false, mat.identity() );
+      gl.uniformMatrix4fv(transformLocation, false, finalTransform);
       gl.uniform4fv(colorLocation, object.color);
-      gl.drawArrays(object.primitive, 0, object.data.length / object.data.dims);
+      gl.drawArrays(object.primitive, 0, object.data.length / object.dims);
     }
 
     drawObject(coordsSys);
@@ -104,15 +112,17 @@ function main() {
 
   }
 
+  var functions = {};
+  functions["x^2+z^2"] = function(x,y){return x*x + y * y};
+  functions["sin(x)"] = function(x,y){return Math.sin(x)};
 
-  // controls
   var funcSel = document.getElementById("functionSelect");
   var minXInput = document.getElementById("minX");
   var maxXInput = document.getElementById("maxX");
   var minZInput = document.getElementById("minZ");
   var maxZInput = document.getElementById("maxZ");
   var init = function(){
-    let func = function(x,y){return x*x + y*y;};
+    let func = functions[funcSel.value];
     let minX = parseInt(minXInput.value);
     let maxX = parseInt(maxXInput.value);
     let minZ = parseInt(minZInput.value);
@@ -126,13 +136,52 @@ function main() {
     let midZ = (minZ + maxZ) / 2;
     coordsSys.setData(minX,maxX,minY,maxY,minZ,maxZ);
     let translation = mat.translation(-midX,-midY,-midZ);
-    let scale = mat.scale(1/(midX - minX),1/(midY - minY),1/(midZ - minZ));
+    let k = 1;
+    let scale = mat.scale(k * 2/(maxX - minX),k * 2/(maxY - minY),k * 2/(maxZ - minZ));
     transform = mat.multiply(scale,translation);
+    /*
+    let tilt = mat.xRotation(-0.1);
+    transform = mat.multiply(tilt,transform)
+    */
     redraw();
   }
-  document.getElementById("drawButton").onClick = init;
+  document.getElementById("drawButton").onclick = init;
 
   init();
+
+  // controls
+  document.addEventListener('keypress', (event) => {
+
+  const key = event.key;
+  switch(key){
+    case 'w':
+      transform = mat.multiply( mat.xRotation(0.1) , transform);
+      break;
+    case 's':
+      transform = mat.multiply( mat.xRotation(-0.1) , transform);
+      break;
+    case 'a':
+      transform = mat.multiply( mat.yRotation(0.1) , transform);
+      break;
+    case 'd':
+      transform = mat.multiply( mat.yRotation(-0.1) , transform);
+      break;
+    case 'q':
+      // movement.roll(1);
+      break;
+    case 'e':
+      // movement.roll(-1);
+      break;
+    case ' ':
+      // movement.forward();
+      break;
+    default:
+      return;
+  }  
+  redraw();
+  return;
+
+}, false);
 }
 
 main();
